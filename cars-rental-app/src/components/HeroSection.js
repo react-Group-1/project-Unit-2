39c556dcdabfcbf984195115805f39c7a641aa13
aaldosari1 <dosr@live.com>
2021-11-08 09:32:15 +0300

    add the hero section

--- cars-rental-app/src/components/HeroSection.js
+++ cars-rental-app/src/components/HeroSection.js
@@ -1,8 +1,49 @@
+import Carousel from 'react-bootstrap/Carousel'
 
 function HeroSection() {
     return (
       <>
-      <h1>Hero section</h1>
+
+<Carousel fade>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="./images/Lexus IS.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="./images/LC500_HB.jpg"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="./images/Land-Rover.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+      
+
       </>
     );
   }
