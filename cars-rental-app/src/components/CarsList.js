37349b3823048802ea0ca651e0e4cab90de2021f
Faisal-Alsharif <ifaisal.alsharif@gmail.com>
2021-11-10 22:24:12 +0300

    fixing cars cards to be responsive

--- cars-rental-app/src/components/CarsList.js
+++ cars-rental-app/src/components/CarsList.js
@@ -20,22 +20,20 @@ function CarsList({id, name, brand,year, price, img, carDelete}) {
 
     return (
       <div className="carDetails">
-        <div className="leftSide">
+      
+         <div className="leftSide">
           <div >
             <img className="carImage" src={"./CarsImage/"+img} alt="car" height="150px" width="250px" />
           </div>
           <div className="details">
             <h3>{brand} {name}</h3>
             <h5>{year}</h5>
-            <h6>ID:{id}</h6>
           </div>
         </div>
         <div className="rightSide">
-          
             <h3>{price} SAR /<small style={{ fontSize:"15px"}}>per day</small></h3>
-            {carDelete && true ? <Button  onClick={deletefromCart} variant="danger" >Delete</Button> : <Button onClick={goTocart} variant="warning">BOOK NOW</Button>}
-            
-        </div>
+            {carDelete && true ? <Button  onClick={deletefromCart} variant="danger" >Delete</Button> : <Button onClick={goTocart} variant="warning">Book now</Button>}
+        </div> 
       </div>
     );
   }
