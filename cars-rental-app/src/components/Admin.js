b7e7e323cbf8a4a003ccace5e76310f19bc46dd7
Faisal-Alsharif <ifaisal.alsharif@gmail.com>
2021-11-10 14:19:09 +0300

    create Admin Component and add functionality to it and handel the changes when admin is logged in

--- cars-rental-app/src/components/Admin.js
+++ cars-rental-app/src/components/Admin.js
@@ -1,7 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import "./Admin.css"
 
 function Admin() {
+
+  const navigate = useNavigate();
     return (
       <>
+      <div className="admin-container">
+        <div className="header">
+          <h1>Admin</h1>
+        </div>
+        <div className="body">
+          <div className="body-left">
+            <h2>Users</h2>
+            <img onClick={()=>{navigate("/Users")}} src="./adminImages/users.png" height="200px" width="250px"></img>
+          </div>
+          <div className="body-right">
+            <h2>Orders</h2>
+            <img onClick={()=>{navigate("/Orders")}} src="./adminImages/orders.png" height="200px" width="250px"></img>
+          </div>
+        </div>
+      </div>
       </>
     );
   }
